4555dbbf4d7965480d3347244cd10d88f7f0eb5b
Votre-NomComplet <I1-P272@Pinfo1.local>
2026-06-04 09:24:33 +0000

    Modification de la methode arrange() et implémentation de booksInBookShelfAreInInsertionOrderAfterCallingArrange

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,6 +1,7 @@
 package bookstoread;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookShelf {
     private final List<String> books = new ArrayList<>();
@@ -14,7 +15,6 @@ public class BookShelf {
     }
 
     public List<String> arrange() {
-        books.sort(Comparator.naturalOrder());
-        return books;
+        return books.stream().sorted().collect(Collectors.toList());
     }
 }
